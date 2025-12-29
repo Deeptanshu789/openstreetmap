48abf61a911481591425f1dab6cc3d8342679933
Deeptanshu Nayak <deeptanshunayak07@gmail.com>
2025-12-29 17:34:00 +0530

    Refactor OpenStreetMap screen layout and styles

--- lib/openstreetmap.dart
+++ lib/openstreetmap.dart
@@ -5,55 +5,50 @@ import 'package:latlong2/latlong.dart';
 
 class Openstreetmapscreen extends StatefulWidget {
   const Openstreetmapscreen({super.key});
+
   @override
   State<Openstreetmapscreen> createState() => _OpenstreetmapScreenState();
 }
 
-
 class _OpenstreetmapScreenState extends State<Openstreetmapscreen> {
   final MapController _mapcontroller = MapController();
-   @override
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       foregroundColor: const Color.fromARGB(255, 58, 162, 226),
-        title: const Text("Open Street Map"),
-          backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        title: const Text("My Location"),
+        backgroundColor: Colors.indigo,
       ),
-      body: Stack(
-        children : [
-         FlutterMap(
-           mapController: _mapcontroller,
-          options: MapOptions(
-            initialCenter: LatLng(0, 0),
-            initialZoom: 3.0,
-            minZoom: 2.0,
-            maxZoom: 100.0,
-          ),
-          children: [
-            TileLayer(
-              urlTemplate: "https://tile.opentopomap.org/{z}/{x}/{y}.png",
-              subdomains: ['a', 'b', 'c'],
-            ),
-            CurrentLocationLayer(
-             style: const LocationMarkerStyle(
-              marker: DefaultLocationMarker(
-                markerSize: Size(35, 35),
-              markerDirection: MarkerDirection.heading,
-                child: Icon(
-                  Icons.location_pin,
-                  color: Colors.blue,
-                              child: Icon(
-                Icons.location_pin,
-                color: Colors.blue,
-              ),
-            ),
-            ),
-         ) ,
-          ],
+      body: FlutterMap(
+        mapController: _mapcontroller,
+        options: MapOptions(
+          initialCenter: LatLng(0, 0),
+          initialZoom: 3.0,
+          minZoom: 2.0,
+          maxZoom: 18.0,
         ),
-        ],
+        children: [
+          TileLayer(
+            urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
           ),
-        );
+          CurrentLocationLayer(
+            style: const LocationMarkerStyle(
+              marker: DefaultLocationMarker(
+                color: Color.fromARGB(255, 255, 86, 86),
+              ),
+              markerSize: Size(35, 35),
+              markerDirection: MarkerDirection.heading,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _mapcontroller.move(LatLng(0, 0), 15),
+        backgroundColor: Colors.indigo,
+        child: const Icon(Icons.my_location, color: Colors.white),
+      ),
+    );
   }
 }
